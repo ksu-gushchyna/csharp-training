b7e574e1aa10b66e25e7862639a372963947c0d6
ksu-gushchyna <ogushchy@OGUSHCHY-UA.us.oracle.com>
2020-01-27 19:17:58 +0200

    my proj

--- MyProject/MyProject/Program.cs
+++ MyProject/MyProject/Program.cs
@@ -5,12 +5,31 @@ namespace MyProject
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			//Console.ReadLine();
+        {
+            float a = 10;
+            float b = 5;
+            float resultA = 0;
+            float resultB = 0;
+            float currentResultA;
+            float currentResultB;
+            int mainResult;
 
-			Class1 class1 = new Class1(3, -5);
-			
-			Console.WriteLine(class1.GetSquare());
-		}
+            for (int i = 1, j = 0; i <= a; i++, j++)
+            {
+                
+                if (a % i == 0 & b % j == 0)
+                {
+                    Console.WriteLine(i + "and" + j);
+                }
+                Console.ReadLine();
+            }
+            
+
+
+
+            //Class1 class1 = new Class1(3, -5);
+
+            //Console.WriteLine(class1.GetSquare());
+        }
 	}
 }
