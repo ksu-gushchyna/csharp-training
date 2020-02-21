6cb082e53e0c71b9846e6f0101493ba0d0196bde
Oksana Gushchyna <oksana.gushchyna@oracle.com>
2020-02-21 18:23:08 +0200

    rectangle

--- AbstractClasses/AbstractClasses/Program.cs
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -9,7 +9,11 @@ namespace AbstractClasses
             Console.WriteLine("Hello World!");
 
             Triangle triangle = new Triangle(1, 2, 3, 1);
-            Console.WriteLine(triangle.GetArea());
+            Console.WriteLine("Triangle Area" + triangle.GetArea());
+
+            Rectangle rectangle = new Rectangle(2, 3);
+            Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+
             Console.ReadLine();
 
         }
---
+++ AbstractClasses/AbstractClasses/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿namespace AbstractClasses
+{
+    //наследование
+    class Rectangle : Figure
+    {
+        private double a;
+        private double b;
+
+        //конструктор
+        public Rectangle(double sideA, double sideB)
+        {
+            this.a = sideA;
+            this.b = sideB;
+        }
+        //полиморфизм
+        internal override double GetArea()
+        {
+            return a * b;
+        }
+
+        protected override double GetPerimeter()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
