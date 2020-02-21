624f0c7747789e9b21614bc7a12bb668443eddef
Oksana Gushchyna <oksana.gushchyna@oracle.com>
2020-02-21 17:23:01 +0200

    unit tests

--- MyProject/UnitTestProject1/GcdByEuclideanAlgorithmWithRecursionTest.cs
+++ MyProject/UnitTestProject1/GcdByEuclideanAlgorithmWithRecursionTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyProject;
 
 namespace UnitTestProject1
@@ -8,10 +7,12 @@ namespace UnitTestProject1
     public class GcdByEuclideanAlgorithmWithRecursionTest
     {
         [TestMethod]
-        public void GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_first_number_more_than_second_one()
+        public void
+            GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_first_number_more_than_second_one()
         {
             //arrange
-            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion = new GcdByEuclideanAlgorithmWithRecursion();
+            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion =
+                new GcdByEuclideanAlgorithmWithRecursion();
             int firstNumb = 106;
             int secondNumb = 16;
             int expected = 2;
@@ -21,14 +22,15 @@ namespace UnitTestProject1
 
             //assert
             Assert.AreEqual(expected, actual, "not equal");
-
         }
 
         [TestMethod]
-        public void GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_second_number_more_than_first_one()
+        public void
+            GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_second_number_more_than_first_one()
         {
             //arrange
-            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion = new GcdByEuclideanAlgorithmWithRecursion();
+            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion =
+                new GcdByEuclideanAlgorithmWithRecursion();
             int firstNumb = 16;
             int secondNumb = 106;
             int expected = 2;
@@ -41,10 +43,12 @@ namespace UnitTestProject1
         }
 
         [TestMethod]
-        public void GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_first_and_second_numbers_are_equal()
+        public void
+            GcdByEuclideanAlgorithmWithRecursion_should_return_correct_value_when_first_and_second_numbers_are_equal()
         {
             //arrange
-            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion = new GcdByEuclideanAlgorithmWithRecursion();
+            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion =
+                new GcdByEuclideanAlgorithmWithRecursion();
             int firstNumb = 10;
             int secondNumb = 10;
             int expected = 10;
@@ -60,16 +64,15 @@ namespace UnitTestProject1
         public void GcdByEuclideanAlgorithmWithRecursion_should_trhow_exception_for_zero()
         {
             //arrange
-            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion = new GcdByEuclideanAlgorithmWithRecursion();
+            GcdByEuclideanAlgorithmWithRecursion gcdByEuclideanAlgorithmWithRecursion =
+                new GcdByEuclideanAlgorithmWithRecursion();
             int firstNumb = 0;
             int secondNumb = 10;
 
             //act
             //assert
-            Assert.ThrowsException<System.DivideByZeroException>(() => gcdByEuclideanAlgorithmWithRecursion.GetGcd(firstNumb, secondNumb));
-
+            Assert.ThrowsException<System.DivideByZeroException>(() =>
+                gcdByEuclideanAlgorithmWithRecursion.GetGcd(firstNumb, secondNumb));
         }
-
-
     }
 }
